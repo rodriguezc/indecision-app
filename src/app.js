d109450ec3fa1dee15709c7a805d64398d3484be
rodriguc <crodriguez@groupemutuel.ch>
2017-09-27 08:40:34 +0200

    end of lesson 10

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 console.log("App.js is running");
 
 // JSX = JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+var templateTwo = (
+  <div>
+    <h1>Andrew Mead</h1>
+    <p>Age: 33</p>
+    <p>Location: Lausanne</p>
+  </div>
+);
+
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+//ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
