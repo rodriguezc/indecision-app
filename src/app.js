6edfe1c04a7958b4118949af97fba0e80f9186d5
rodriguc <crodriguez@groupemutuel.ch>
2017-09-28 07:20:47 +0200

    session13

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ console.log("App.js is running");
 
 var app = {
   title: "Indecision App",
-  subtitle: "Put your life in the hands of a computer"
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
 };
 
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -19,27 +21,23 @@ var template = (
 var user = {
   name: "Andrew",
   age: 26,
-  location: "New York"
+  location: "Philadelphia"
 };
-var userName = "Andrew";
-var userAge = 27;
-var userLocation = "Philadelphia";
 
 function getLocation(location) {
   if (location) {
     return <p>Location: {location}</p>;
   }
-  return undefined;
 }
 
 var templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
     {getLocation(user.location)}
   </div>
 );
 
 var appRoot = document.getElementById("app");
 //ReactDOM.render(template, appRoot);
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
