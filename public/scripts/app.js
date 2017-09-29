4c3d1903b9ec2e2d16cf051171fcd1d780bc137a
rodriguc <crodriguez@groupemutuel.ch>
2017-09-29 13:24:58 +0200

    end of lesson 24

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,39 +1,39 @@
 "use strict";
 
-var visibiltiy = false;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var toggleVisbility = function toggleVisbility() {
-  visibiltiy = !visibiltiy;
-  render();
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var appRoot = document.getElementById("app");
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Anonymous";
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var render = function render() {
-  var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      "Visible toggle"
-    ),
-    React.createElement(
-      "button",
-      { onClick: toggleVisbility },
-      visibiltiy ? "Hide details" : "Show details"
-    ),
-    visibiltiy && React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "p",
-        null,
-        "Hey. These are some details you can now see!"
-      )
-    )
-  );
-  console.log(ReactDOM.render(template, appRoot));
-};
+    _classCallCheck(this, Person);
 
-render();
+    this.name = name;
+    this.age = age;
+  }
+
+  _createClass(Person, [{
+    key: "getGreeting",
+    value: function getGreeting() {
+      // return "Hi. I am  " + this.name + "!";
+      return "Hi. I am " + this.name + "!";
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      // return "Hi. I am  " + this.name + "!";
+      return this.name + " is " + this.age + " year(s) old.";
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person("Christophe Rodriguez", 19);
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getDescription());
