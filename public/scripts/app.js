ddad23f5ed357725c31098780c3136bc94126ef5
rodriguc <crodriguez@groupemutuel.ch>
2017-09-29 07:59:01 +0200

    sesison 19

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,42 +5,82 @@ console.log("App.js is running");
 var app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of a computer",
-  options: ["One", "Two"]
+  options: []
 };
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.options.length > 0 ? "Here are your options" : "No options"
-  ),
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "Item one"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Item two"
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  if (option) {
+    console.log(option);
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+var render = function render() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "p",
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "ol",
+      null,
+      React.createElement(
+        "li",
+        null,
+        "Item one"
+      ),
+      React.createElement(
+        "li",
+        null,
+        "Item two"
+      )
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        { type: "submit" },
+        "Add option"
+      ),
+      React.createElement(
+        "button",
+        { type: "button", onClick: onRemoveAll },
+        "Remove all"
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+render();
--- src/app.js
+++ src/app.js
@@ -3,20 +3,50 @@ console.log("App.js is running");
 const app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of a computer",
-  options: ["One", "Two"]
+  options: []
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = e => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if (option) {
+    console.log(option);
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
 
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+      <p>{app.options.length}</p>
+
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button type="submit">Add option</button>
+        <button type="button" onClick={onRemoveAll}>
+          Remove all
+        </button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+render();
