b766012a944a8f0d86d4f3b18707806adceb630f
rodriguc <crodriguez@groupemutuel.ch>
2017-10-10 12:03:16 +0200

    end of lesson 53

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+//import validator from "validator";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = React.createElement("p", {}, "testing 123");
+
+ReactDOM.render(template, document.getElementById("app"));
+
+//console.log(validator.isEmail("test@gmail.com"));
+
 //import "./utils.js";
 
 //import subtract, { square, add } from "./utils.js";
@@ -6,13 +17,13 @@
 //console.log(add(100, 23));
 //console.log(subtract(100, 81));
 
-import isSenior, { isAdult, canDrink } from "./person";
+//import isSenior, { isAdult, canDrink } from "./person";
 
-console.log("17 years old is an andult: ", isAdult(17));
-console.log("18 years old is an adult: ", isAdult(18));
+//console.log("17 years old is an andult: ", isAdult(17));
+//console.log("18 years old is an adult: ", isAdult(18));
 
-console.log("20 years old is an andult: ", canDrink(20));
-console.log("21 years old is an andult: ", canDrink(21));
+//console.log("20 years old is an andult: ", canDrink(20));
+//console.log("21 years old is an andult: ", canDrink(21));
 
-console.log("6 years old is senior", isSenior(64));
-console.log("65 years old is senior", isSenior(65));
+//console.log("6 years old is senior", isSenior(64));
+//console.log("65 years old is senior", isSenior(65));
