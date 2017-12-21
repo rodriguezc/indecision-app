1030e7e69a6be7ba99a9cabfe297c4a26d04eec7
rodriguc <crodriguez@groupemutuel.ch>
2017-12-21 11:34:53 +0100

    end of lesson 58

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,18 +1,14 @@
 import React from "react";
 
 export default class AddOption extends React.Component {
+  state = { error: undefined };
+
   constructor(props) {
     super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
   }
 
-  handleAddOption(e) {
+  handleAddOption = e => {
     e.preventDefault();
-    alert("ICI");
-
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
     this.setState(() => ({ error }));
@@ -20,7 +16,7 @@ export default class AddOption extends React.Component {
     if (!error) {
       e.target.elements.option.value = "";
     }
-  }
+  };
 
   render() {
     return (
