e4374e17b9cc137a015c2ecf4636bd02fe4ba8e7
rodriguc <crodriguez@groupemutuel.ch>
2017-12-21 13:57:39 +0100

    end of lesson 64

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
+
 module.exports = {
-  entry: "./src/app.js", // where webpack looks to start building the bundle
+  entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
@@ -9,13 +10,16 @@ module.exports = {
     rules: [
       {
         loader: "babel-loader",
-        test: /\.js$/, //babel-loader processe uniquement ces fichiers
-        exclude: /node_modules/ //On exclut ce répertoire
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
   devtool: "cheap-module-eval-source-map",
-
   devServer: {
     contentBase: path.join(__dirname, "public")
   }
